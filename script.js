d9ef23030c38a972fc71ca9ccd2db006ffc07cae
William Eckerleben <67724687+weckerleben@users.noreply.github.com>
2024-06-05 19:03:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date(process.env.COUNTDOWN_DATE).getTime();
+var countDownDate = new Date("Jul 17, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
